67809fca97d5f3feff81148369ef2990ff0b5598
Seva41 <sebadinator@gmail.com>
2023-09-20 19:38:37 -0300

    basura final

--- CTF/CTF3/basura.py
+++ CTF/CTF3/basura.py
@@ -8,21 +8,21 @@ with open(image_path, "rb") as image_file:
 # Define los paquetes a enviar (ethernet, ip, tcp, datos)
 eth = Ether(dst="fc:f8:ae:33:44:55", src="fc:f1:36:dd:ee:ff")
 ip = IP(
-    src="192.168.0.25",
+    src="192.168.0.10",
     dst="192.168.0.1",
     ttl=64,
-    flags="DF",
+    flags="DF",  # Set the Don't Fragment (DF) flag
     id=32711,
-    len=1200,
+    len=0,  # Set the length to 0 for automatic calculation
     chksum=0,
 )
 
-# Crea el paquete TCP
+# Crea el paquete TCP with a valid seq number
 tcp = TCP(
-    sport=12346,
+    sport=12345,
     dport=80,
     flags="S",
-    seq=1800,
+    seq=1000,  # Set a valid 32-bit sequence number
     options=[("Timestamp", (0, 0))],
     chksum=0,
 )
@@ -30,15 +30,13 @@ tcp = TCP(
 # Define el paquete completo
 packet = eth / ip / tcp / Raw(load=image_data)
 
-# Borra los checksums para que Scapy los calcule automáticamente
-del packet[IP].chksum
-del packet[TCP].chksum
-
-# Recalcula los checksums
+# Calculate the TCP checksum
+packet[TCP].chksum = 0  # Clear the existing checksum field
 packet = packet.__class__(bytes(packet))
+packet[TCP].chksum = None  # Recalculate the checksum
 
 # Define el path del archivo .pcap
-output_pcap = "basura.pcap"
+output_pcap = "basura1.pcap"
 
 # Escribe el paquete en el archivo .pcap
 wrpcap(output_pcap, packet)
@@ -52,21 +50,21 @@ with open(image_path, "rb") as image_file:
 # Define los paquetes a enviar (ethernet, ip, tcp, datos)
 eth = Ether(dst="fc:f8:ae:33:44:55", src="fc:64:ba:cc:ee:ff")
 ip = IP(
-    src="192.168.0.41",
+    src="192.168.0.10",
     dst="192.168.0.1",
     ttl=64,
-    flags="DF",
-    id=32731,
-    len=1200,
+    flags="DF",  # Set the Don't Fragment (DF) flag
+    id=32711,
+    len=0,  # Set the length to 0 for automatic calculation
     chksum=0,
 )
 
-# Crea el paquete TCP
+# Crea el paquete TCP with a valid seq number
 tcp = TCP(
-    sport=12326,
+    sport=12345,
     dport=80,
     flags="S",
-    seq=2700,
+    seq=1000,  # Set a valid 32-bit sequence number
     options=[("Timestamp", (0, 0))],
     chksum=0,
 )
@@ -74,12 +72,10 @@ tcp = TCP(
 # Define el paquete completo
 packet = eth / ip / tcp / Raw(load=image_data)
 
-# Borra los checksums para que Scapy los calcule automáticamente
-del packet[IP].chksum
-del packet[TCP].chksum
-
-# Recalcula los checksums
+# Calculate the TCP checksum
+packet[TCP].chksum = 0  # Clear the existing checksum field
 packet = packet.__class__(bytes(packet))
+packet[TCP].chksum = None  # Recalculate the checksum
 
 # Define el path del archivo .pcap
 output_pcap = "basura2.pcap"
@@ -96,21 +92,21 @@ with open(image_path, "rb") as image_file:
 # Define los paquetes a enviar (ethernet, ip, tcp, datos)
 eth = Ether(dst="fc:f8:ae:33:44:55", src="f8:a9:d0:dd:ab:ff")
 ip = IP(
-    src="192.168.0.41",
+    src="192.168.0.10",
     dst="192.168.0.1",
     ttl=64,
-    flags="DF",
-    id=32731,
-    len=1200,
+    flags="DF",  # Set the Don't Fragment (DF) flag
+    id=32711,
+    len=0,  # Set the length to 0 for automatic calculation
     chksum=0,
 )
 
-# Crea el paquete TCP
+# Crea el paquete TCP with a valid seq number
 tcp = TCP(
-    sport=12326,
+    sport=12345,
     dport=80,
     flags="S",
-    seq=0,
+    seq=1000,  # Set a valid 32-bit sequence number
     options=[("Timestamp", (0, 0))],
     chksum=0,
 )
@@ -118,12 +114,10 @@ tcp = TCP(
 # Define el paquete completo
 packet = eth / ip / tcp / Raw(load=image_data)
 
-# Borra los checksums para que Scapy los calcule automáticamente
-del packet[IP].chksum
-del packet[TCP].chksum
-
-# Recalcula los checksums
+# Calculate the TCP checksum
+packet[TCP].chksum = 0  # Clear the existing checksum field
 packet = packet.__class__(bytes(packet))
+packet[TCP].chksum = None  # Recalculate the checksum
 
 # Define el path del archivo .pcap
 output_pcap = "basura3.pcap"
