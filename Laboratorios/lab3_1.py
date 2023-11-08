329641948cbf2f42b2606983b321dee2ec1070a0
Sebastián Dinator <sebadinator@gmail.com>
2023-11-07 23:11:49 -0300

    lab3

---
+++ Laboratorios/lab3_1.py
@@ -0,0 +1,16 @@
+def verificar_contraseña(entrada_usuario):
+    contraseña_secreta = "s3cr3t!"
+    return entrada_usuario == contraseña_secreta
+
+
+def main():
+    entrada_usuario = input("Introduce la contraseña: ")
+    if verificar_contraseña(entrada_usuario):
+        print("¡Contraseña correcta!")
+        print("La bandera es FLAG{bien_hecho}")
+    else:
+        print("Contraseña incorrecta.")
+
+
+if __name__ == "__main__":
+    main()
